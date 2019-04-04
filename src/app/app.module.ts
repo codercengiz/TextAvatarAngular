8a8ab91f8f1a9421557a9cb7aa724e0c241b767f
cengiz <codercengiz@gmail.com>
2019-04-04 12:19:50 +0200

    directive added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AvatartextDirective } from 'src/directives/avatartext.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AvatartextDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/directives/avatartext.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AvatartextDirective } from './avatartext.directive';
+
+describe('AvatartextDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AvatartextDirective();
+    expect(directive).toBeTruthy();
+  });
+});
